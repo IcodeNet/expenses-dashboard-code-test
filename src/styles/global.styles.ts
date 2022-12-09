f6879fc47b255d921a4821477f2dd48ae0cf8439
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-09 21:58:56 +0000

    Adds Container component. Updates global styles.

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from "styled-components";
+import { TYPOGRAPHY } from "./primitives";
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    font-family: 'Open Sans', sans-serif;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    padding: 2rem 0;
+    margin: 0;
+    background-color: #f3f3f3;
+    font-family: ${TYPOGRAPHY.base};
+  }
+
+  #root {
+    flex-grow: 1;
   }
 `;
