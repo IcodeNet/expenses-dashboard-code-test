80de2c5f330e5efdd48561c669d7a700ecc83fa2
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-13 14:41:56 +0000

    Refactors formatCurrency utility.

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -2,6 +2,8 @@ import { CURRENCY_SYMBOL, CurrencyCodeIso } from "../api/types/transactions";
 
 export const formatCurrency = (value: number, currencyCode: CurrencyCodeIso, prependIndicator?: boolean) => {
     const indicator = prependIndicator && value >= 0 ? "+" : "";
+    const currencySymbol = CURRENCY_SYMBOL[currencyCode];
+    const formattedVal = Math.abs(value).toFixed(2);
 
-    return `${indicator}${CURRENCY_SYMBOL[currencyCode]}${Math.abs(value).toFixed(2)}`;
+    return [indicator, currencySymbol, formattedVal].join("");
 }
