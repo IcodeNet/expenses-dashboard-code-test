8e90cfe7c03cfc33698cd4d6e93297cb9826f73b
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-14 08:51:17 +0000

    Resolves webpack double initialising app.

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./public/index.html")
+      template: path.resolve(__dirname, "./public/index.html"),
+      inject: false
     }),
   ],
 };
